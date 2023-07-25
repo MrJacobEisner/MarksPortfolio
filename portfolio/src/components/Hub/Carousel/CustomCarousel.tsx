06f3b5838eeadb5e58bc2284fd78abfe90bc3963
MrJacobEisner <mrjeisner@gmail.com>
2023-07-25 19:33:33 -0400

    WIP

--- portfolio/src/components/Hub/Carousel/CustomCarousel.tsx
+++ portfolio/src/components/Hub/Carousel/CustomCarousel.tsx
@@ -22,7 +22,6 @@ const CustomCarousel = (CustomCarouselProps: CustomCarouselProps) => {
                         className="Image"
                         src={require(`../../../assets/gallery/${image.imageName}`)}
                         alt=""
-                        loading="lazy"
                     />
                     <Carousel.Caption>
                         <div className="Caption-Wrapper">
